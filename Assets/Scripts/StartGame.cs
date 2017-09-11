954c98d7af19263d74e5748c28e9284654afb96d
Lucas Selani de Melo <lucasselani@inatel.br>
2017-09-11 16:32:44 -0300

    criando menu inicial

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour, IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPointerClick(PointerEventData eventData) // 3
+	{
+		print("I was clicked");
+		SceneManager.LoadScene ("BattleScene");
+	}
+
+}
